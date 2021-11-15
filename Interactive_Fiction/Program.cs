e80f791b4127cfca88e3e4241833a7b405684f0b
MatthewMadore902 <90638212+MatthewMadore902@users.noreply.github.com>
2021-11-15 13:48:56 -0400

    Update Program.cs

--- Interactive_Fiction/Program.cs
+++ Interactive_Fiction/Program.cs
@@ -8,8 +8,49 @@ namespace Interactive_Fiction
 {
 	class Program
 	{
+		static string[] story = new string[11];
+		
 		static void Main(string[] args)
 		{
+			Pages();
+			Console.WriteLine(story[1]);
+			Console.ReadKey();
+		}
+
+		static void GameLoop()
+		{
+			
+		}
+
+		static void Pages()
+		{
+			string userInput = Console.ReadLine();
+
+			story[0] = "Title Screen";
+			story[1] = "Page 1, You wake up in the middle of no where unsure how or why you're here,as you start to regan you ability to stand you begin to analysis your suroundings all old dead tree's and a few boulders,you find an old hatchet beside you";
+			//string[] page = story[1].Split(' ');
+			story[2] = "Page 2";
+			story[3] = "Page 3";
+			story[4] = "Page 4";
+			story[5] = "Page 5";
+			story[6] = "Page 6";
+			story[7] = "Page 7";
+			story[8] = "Page 8";
+			story[9] = "Page 9";
+			story[10] = "Page 10";
+
+
+		}
+
+
+		
+
+
+
+		static void Choice()
+		{
+			
 		}
 	}
+
 }
