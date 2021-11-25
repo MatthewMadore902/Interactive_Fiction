6a06948bad9ce45b895628d3230ce8a42ce73b4e
MatthewMadore902 <90638212+MatthewMadore902@users.noreply.github.com>
2021-11-25 15:41:58 -0400

    added user input and test story

--- Interactive_Fiction/Program.cs
+++ Interactive_Fiction/Program.cs
@@ -11,70 +11,104 @@ namespace Interactive_Fiction
 		static char[] splits = {';'};
 		static string[] story = new string[11];
 		static bool gameOver = false;
-		static string userInput = Console.ReadLine();
+		static int currentpage;
+		static string plot;
+		static string option;
+		static string choice1;
+		static string choice2;
+		static int Parse1;
+		static int Parse2;
+		static string playerInput;
 		
 		static void Main(string[] args)
 		{
-			Pages();
-			//Console.WriteLine(story[1]);
-			Console.ReadKey();
-			Console.ReadLine();
-			Pages();
-
+			GameLoop();
 		}
 
 		static void GameLoop()
 		{
-			string userInput = Console.ReadLine();
-
 			while (gameOver == false)
 			{
-
+				Story();
+				PageSplit();
+				PlayerInput();
+				Story();
+				PageSplit();
+				PlayerInput();
 			}
 			/*if (userInput == Choice(story[]))
 			{ }*/
 		}
 
-		static void Pages()
+		static void Story()
 		{
-		
-
-			story[0] = "Title Screen";
-			story[1] = "Page 1,You wake up in the middle of no where unsure how or why you're here, as you start to regan you ability to stand;you begin to analysis your suroundings all old dead tree's and a few boulders.;2;3";
+			story[0] = "Title;Options;2;3";
+			story[1] = "Test1;option;4;3";
 			//string[] page = story[1].Split(splits);
-			story[2] = "Page 2";
-			story[3] = "Page 3";
-			story[4] = "Page 4";
-			story[5] = "Page 5";
-			story[6] = "Page 6";
-			story[7] = "Page 7";
-			story[8] = "Page 8";
-			story[9] = "Page 9";
-			story[10] = "Page 10";
+			story[2] = "Test2;option;2;3";
+			story[3] = "Test3;option;2;3";
+			story[4] = "Test4;option;3;3";
+			story[5] = "Test5;option;2;3";
+			story[6] = "Test6; option; 2; 3";
+			story[7] = "Test7;option;2;3";
+			story[8] = "Test8;option;2;3";
+			story[9] = "Test9;option;2;3";
+			story[10] = "Test10;option;2;3";
 
-			Console.WriteLine(story[1]);
+			//Console.WriteLine(story[1]);
 		}
 
-
-		
-
-
-
-		static void Choice(string[] story)
+		static void PageSplit()
 		{
-			if (story[1] == userInput)
-			{
-				if (userInput == "A")
-				{
-					Console.WriteLine(story[2]);
-					Console.ReadKey();
-				}
-				if (userInput == "B")
-				{
-					
-				}
-			}
+
+			string[] contents = story[currentpage].Split(';');
+
+			plot = contents[0];
+			option = contents[1];
+			choice1 = contents[2];
+			choice2 = contents[3];
+			Console.WriteLine(plot);
+			Console.WriteLine(option);
+			Console.WriteLine(choice1);
+			Console.WriteLine(choice2);
+
+			Parse1 = int.Parse(contents[2]);
+			Parse2 = int.Parse(contents[3]);
 		}
+
+		/*static void Choice(string[] story)
+		  {
+			  if (story[1] == userInput)
+			  {
+				  if (userInput == "A")
+				  {
+					  Console.WriteLine(story[2]);
+					  Console.ReadKey();
+				  }
+				  if (userInput == "B")
+				  {
+
+				  }
+			  }
+		  }*/
+
+		static void PlayerInput()
+		{
+		
+			playerInput = Console.ReadLine();
+			switch (playerInput)
+			{
+				case "a":
+					currentpage = Parse1;
+					break;
+
+				case "b":
+					currentpage = Parse2;
+					break;
+			}
+
+		}
+
 	}
 
 }
