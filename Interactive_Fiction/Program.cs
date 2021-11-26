f0dd68e554b6894b5cdbd0c5e46236b7021af062
MatthewMadore902 <90638212+MatthewMadore902@users.noreply.github.com>
2021-11-26 10:02:52 -0400

    added tests

--- Interactive_Fiction/Program.cs
+++ Interactive_Fiction/Program.cs
@@ -42,25 +42,23 @@ namespace Interactive_Fiction
 
 		static void Story()
 		{
-			story[0] = "Title;Options;2;3";
-			story[1] = "Test1;option;4;3";
-			//string[] page = story[1].Split(splits);
-			story[2] = "Test2;option;2;3";
-			story[3] = "Test3;option;2;3";
-			story[4] = "Test4;option;3;3";
-			story[5] = "Test5;option;2;3";
-			story[6] = "Test6; option; 2; 3";
-			story[7] = "Test7;option;2;3";
-			story[8] = "Test8;option;2;3";
-			story[9] = "Test9;option;2;3";
-			story[10] = "Test10;option;2;3";
-
-			//Console.WriteLine(story[1]);
+			// this is what the contents number changes to 
+			//                        ↓
+			story[0] = "Title;Options;0;10";
+			story[1] = "Test1;option;1;9";
+			story[2] = "Test2;option;2;8";
+			story[3] = "Test3;option;3;7";
+			story[4] = "Test4;option;4;6";
+			story[5] = "Test5;option;5;5";
+			story[6] = "Test6;option;6;4";
+			story[7] = "Test7;option;7;3";
+			story[8] = "Test8;option;8;2";
+			story[9] = "Test9;option;9;1";
+			story[10] = "Test10;option;10;0";
 		}
 
 		static void PageSplit()
 		{
-
 			string[] contents = story[currentpage].Split(';');
 
 			plot = contents[0];
@@ -72,29 +70,17 @@ namespace Interactive_Fiction
 			Console.WriteLine(choice1);
 			Console.WriteLine(choice2);
 
+			//everytime the user inputs a or b the number changes depenin f on what number a and b are (contents [1] < this number
 			Parse1 = int.Parse(contents[2]);
 			Parse2 = int.Parse(contents[3]);
 		}
 
-		/*static void Choice(string[] story)
-		  {
-			  if (story[1] == userInput)
-			  {
-				  if (userInput == "A")
-				  {
-					  Console.WriteLine(story[2]);
-					  Console.ReadKey();
-				  }
-				  if (userInput == "B")
-				  {
-
-				  }
-			  }
-		  }*/
-
 		static void PlayerInput()
 		{
-		
+			//Parse is turning a string into a number so a = 1 b = 9 or 4, etc.
+			//parse uses user input to determine the number like a or b will change into one of the two numbers specified 
+			//switch case is used for a if/else statment when you want to test and there are 3 or more options
+			//switch statment needs to match, so playerInput is a which will mean current page = whatever a is 
 			playerInput = Console.ReadLine();
 			switch (playerInput)
 			{
